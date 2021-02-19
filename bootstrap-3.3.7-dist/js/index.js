10409fd9f3e10114568e69324038c16caa06016d
Stacey Torres <torress@tcd.ie>
2021-02-19 11:35:41 +0000

    Added a back to top button

--- bootstrap-3.3.7-dist/js/index.js
+++ bootstrap-3.3.7-dist/js/index.js
@@ -1,9 +1,47 @@
-$('.video').parent().click(function() {
-    if ($(this).children(".video").get(0).paused) {
-        $(this).children(".video").get(0).play();
-        $(this).children(".playpause").fadeOut();
+// this is about the back to top button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+        mybutton.style.display = "block";
     } else {
-        $(this).children(".video").get(0).pause();
-        $(this).children(".playpause").fadeIn();
+        mybutton.style.display = "none";
     }
-});
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// $('.video').parent().click(function() {
+//     if ($(this).children(".video").get(0).paused) {
+//         $(this).children(".video").get(0).play();
+//         $(this).children(".playpause").fadeOut();
+//     } else {
+//         $(this).children(".video").get(0).pause();
+//         $(this).children(".playpause").fadeIn();
+//     }
+// });
